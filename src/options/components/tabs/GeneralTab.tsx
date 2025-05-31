c805d58d807b48ab5ccdc3c62fbbd9ee565f2c15
parsaghaffari <parsa.ghaffari@gmail.com>
2025-05-31 19:30:51 +0100

    refactor: implement VerticalTabs component for improved options layout

---
+++ src/options/components/tabs/GeneralTab.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { AboutSection } from '../AboutSection';
+
+export function GeneralTab() {
+  return (
+    <div className="space-y-6">
+      <AboutSection />
+    </div>
+  );
+}
